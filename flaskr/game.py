99b63c6f972e4e2419789d122d57aae2786d9643
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2021-07-12 13:05:02 -0500

    saving before switching to svelte frontend

--- flaskr/game.py
+++ flaskr/game.py
@@ -12,6 +12,8 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from flask_login import current_user, login_required
+from flask_socketio import emit
 from flaskr.db import get_db
 
 from .model.game import Game
@@ -34,9 +36,13 @@ def create():
                 if db.games.find_one({'game_id': game_id}) is None:
                     # no existing game has this key
                     print('creating game with id', game_id)
+                    print(session)
+                    gameboard = initialize_game()
                     record = {'game_id': game_id, 
-                              'players': [(session['user_id'], session['username'])],
-                              'n_players': 1}
+                              'players': [(session['_user_id'], current_user.username)],
+                              'n_players': 1,
+                              **gameboard
+                              }
                     # player = Player(session['username'], session['user_id'])
                     # game = Game(player, game_id)
                     db.games.insert_one(record)
@@ -45,15 +51,24 @@ def create():
         elif 'join' in request.form:
             game_id = request.form['gamecode']
             game = db.games.find_one({'game_id': game_id})
+            if game['n_players'] == 3:
+                initialize_game()
             if game is None:
                 error = "No game exists with that code"
-            elif game.n_players == 3:
+            elif session['_user_id'] in [x[0] for x in game['players']]:
+                print('player re-entering room')
+            elif game['n_players'] == 3:
                 error = "Game is full"
             else:
-                game.players.append((session['user_id'], session['username']))
-                game.n_players += 1
-                print('adding player to game')
-                db.games.update_one({'game_id': game_id}, game)
+                game['players'].append((session['_user_id'], current_user.username))
+                game['n_players'] += 1
+                print('adding player to game', game)
+                db.games.replace_one({'game_id': game_id}, game)
+                if game['n_players'] == 3:
+                    initialize_game()
+
+
+        ## TODO add a leave room option 
 
         if error is None:
             return redirect(url_for(f'.gameboard', id=game_id))
@@ -65,8 +80,46 @@ def create():
 def gameboard(id):
     db = get_db().ddz
     game = db.games.find_one({'game_id': id})
+    print(current_user)
     return render_template('game/game.html', game=game)
 
 @socketio.on('connect')
 def connectionMade():
-    print('Connection occured! with ', session['username'])
+    print('Connection occured! with ', current_user.username, request.sid)
+    
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected', current_user.username)
+
+@socketio.on('my event')
+def handleCustomEvent(data):
+    print('received from client', data)
+    # emit('start game', "Starting the game now")
+
+#
+def initialize_game():
+    deck = []
+    for i in range(4):
+        deck += list(range(3, 16))
+
+    # add jokers 
+    deck.append(16)
+    deck.append(17)
+
+    random.shuffle(deck)
+
+    hands = [sorted(deck[:17]), sorted(deck[17:34]), sorted(deck[34:51]) ]
+    blind = sorted(deck[51:])
+
+    flipped_card = random.randint(0, 51)
+    return {'hands': hands, 'blind': blind, 'flipped_card': flipped_card}
+    # socketio.emit('deal cards', data=hands, broadcast=True)
+    # game = db.games.find_one({'game_id': game_id})
+    # game['cards'] = [] 
+
+### Game state
+# players
+# cards 
+# bids
+# blind 
+# currentPile

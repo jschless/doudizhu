a950d8c471a0bbf94d4f6e9b794fea8d70395ed4
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2021-07-26 06:53:34 -0500

    updated move validation mid round

--- tests/test_validate_move_midround.py
+++ tests/test_validate_move_midround.py
@@ -1,15 +1,14 @@
 import pytest
-import flaskr
 
-def test_single():
-    assert flaskr.game.validate_move(
-        {'hand_type': 'single', 'discard_type': 'no-discard', 
-        'hand_history': [([3], [], [])]}, 
-        [4], []) == ('single', 'no-discard')
+def test_single(test_game):
+    test_game.hand_type = 'single'
+    test_game.discard_type = 'no-discard'
+    test_game.hand_history = [([3], [], [])]
+    assert test_game.validate_move([4], []) == ('single', 'no-discard')
 
-def test_lower_single():
+def test_lower_single(test_game):
+    test_game.hand_type = 'single'
+    test_game.discard_type = 'no-discard'
+    test_game.hand_history = [([3], [], [])]
     with pytest.raises(RuntimeError):
-        assert flaskr.game.validate_move(
-            {'hand_type': 'single', 'discard_type': 'no-discard', 
-            'hand_history': [([4], [], [])]}, 
-            [3], []) == ('single', 'no-discard')
+        assert test_game.validate_move([3], []) == ('single', 'no-discard')

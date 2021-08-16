b9379122292b572738d3382e3190ccc1d32e7c37
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2021-08-16 16:35:07 -0500

    formatting

--- flaskr/socket_utils.py
+++ flaskr/socket_utils.py
@@ -1,12 +1,13 @@
 from . import socketio
 
+
 def send_socket(message: str, data, address=None):
     socketio.emit(message, data, to=address)
 
 
-def flash_message(message: str, address=None, event='flash') -> None:
+def flash_message(message: str, address=None, event="flash") -> None:
     """Displays a notification in red at the top of the client
-    
+
     event: "flash" -> rewrite to flash, "flash append" -> append toflash
     player: pass players dict if you want to flash to a specific user
     """
